507b5b08cecbffbfb11ce507687f01230d14eca8
denismarciano <marcianodio94@gmail.com>
2018-04-18 01:41:47 +0700

    s3 update

--- config/initializers/paperclip.rb
+++
@@ -1,3 +0,0 @@
-Paperclip::Attachment.default_options[:url] = ':https://s3.console.aws.amazon.com/s3/buckets/coastline-goods/?region=us-east-1&tab=overview'
-Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
-Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-east-1.amazonaws.com'
