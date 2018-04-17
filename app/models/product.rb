675e52f19d9e3330a3b138e839c0476b3dcb5902
denismarciano <marcianodio94@gmail.com>
2018-04-18 01:34:35 +0700

    aaa

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ActiveRecord::Base
   belongs_to :category
   has_many :reviews
-  	has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  	has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
   	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
   	validates :name, :description, :price, presence: true
