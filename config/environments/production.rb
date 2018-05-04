4eecaf2d48b82325da4bf51b7af17cf610509ef7
denismarciano <marcianodio94@gmail.com>
2018-05-04 22:57:25 +0700

    sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,17 +78,19 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { host: 'https://coastline-goods94.herokuapp.com' }
+ 
+ # Setup the mailer config
   config.action_mailer.delivery_method = :smtp
-
-ActionMailer::Base.smtp_settings = {
-  :user_name => ENV['SENDGRID_USERNAME'],
-  :password => ENV['SENDGRID_PASSWORD'],
-  :domain => 'heroku.com',
-  :address => 'smtp.sendgrid.net',
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
-}
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'heroku.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 
 #Sets for Amazon S3
 config.paperclip_defaults = {
