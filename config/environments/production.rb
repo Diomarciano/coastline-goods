db2330cb90cad460b805ad3e9772e456de103ed6
denismarciano <marcianodio94@gmail.com>
2018-05-06 14:55:23 +0700

    ah

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,11 +81,11 @@ Rails.application.configure do
 
  # Setup the mailer config
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
+
   config.action_mailer.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'https://coastline-goods94.herokuapp.com',
+    :domain => 'heroku.com',
     :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
